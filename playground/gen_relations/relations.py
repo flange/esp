78925925daf876e4936ca7af046b4f884e8a4233
Frank Lange <langefrq@informatik.hu-berlin.de>
2017-05-09 13:32:46 +0200

    Alignment Generation:   * first working version enumerating all possible relations given     two label sets and for each relation enumerating every possible     alignment pair

--- playground/gen_relations/relations.py
+++ playground/gen_relations/relations.py
@@ -13,6 +13,7 @@ def getBinaryRelations(l1, l2):
   todo = []
   todo.append(product(l1, l2))
 
+
   while (len(todo) > 0):
 
     curr = todo.pop(0)
@@ -76,6 +77,15 @@ def relationMaxComplexity(relation):
 def getComplexity(almPair):
   return len(almPair[0]) * len(almPair[1])
 
+def printRelation(relation):
+  tmp = []
+
+  for corr in relation:
+    tmp.append((set(corr[0]), set(corr[1])))
+
+  print(tmp)
+
+
 
 
 l1 = str(sys.argv[1]).split()
@@ -83,64 +93,79 @@ l2 = str(sys.argv[2]).split()
 
 relations = getBinaryRelations(l1, l2)
 
-#relation = [('a', 'x'), ('a', 'y')]
-#relation = [('a', 'x'), ('a', 'y'), ('b', 'y')]
-#print(relation)
+new_relations = []
 
 for relation in relations:
-  print(relation)
+  new_relation = []
 
-  lhs = set()
-  rhs = set()
+  for corr in relation:
+    new_corr = (frozenset(corr[0]), frozenset(corr[1]))
+    new_relation.append(new_corr)
 
-  for l,r in relation:
-    lhs.add(l)
-    rhs.add(r)
-
-
-  lscombos = list(powerset(lhs))
-  lscombos = list(map(lambda x: list(x), lscombos))
-  lscombos.remove([])
+  new_relations.append(new_relation)
 
 
-  rscombos = list(powerset(rhs))
-  rscombos = list(map(lambda x: list(x), rscombos))
 
-  rscombos.remove([])
+for relation in new_relations:
 
-  almUnion = product(lscombos, rscombos)
-
-  max_k = relationMaxComplexity(relation)
+  printRelation(relation)
 
   almMap = {}
+  almMap[1] = set()
 
-  for pair in almUnion:
-    c = getComplexity(pair)
-    almMap.setdefault(c, []).append(pair)
-
-  for k in range(1, max_k + 1):
-    if k not in almMap:
-      continue
-
-    pairs = almMap[k]
-
-    for pair in pairs:
-      print("  ", k, pair)
-
-    print()
+  # 1er Korrespondenzen
+  for t in relation:
+    almPair = (frozenset(t[0]), frozenset(t[1]))
+    almMap[1].add(almPair)
 
 
+  for corr in almMap[1]:
 
-#  for p in almUnion:
-#    print("  ", p[0], "-", p[1], len(p[0] * len(p[1])))
+    lhs_labels_remaining = frozenset([x for x in l1 if x not in corr[0] ])
+    rhs_labels_remaining = frozenset([x for x in l2 if x not in corr[1] ])
+
+    for l in lhs_labels_remaining:
+      v_corr = (frozenset([l]), corr[1])
+
+      if v_corr in relation:
+
+        new_corr = (corr[0].union(v_corr[0]), corr[1])
+        new_corr_c = getComplexity(new_corr)
+
+        if not new_corr_c in almMap:
+          almMap[new_corr_c] = set()
+
+        if new_corr not in almMap[new_corr_c]:
+          almMap.setdefault(new_corr_c, []).add(new_corr)
 
 
+    for l in rhs_labels_remaining:
+      v_corr = (corr[0], frozenset([l]))
+
+      if v_corr in relation:
+
+        new_corr = (corr[0], corr[1].union(v_corr[1]))
+        new_corr_c = getComplexity(new_corr)
+
+        if not new_corr_c in almMap:
+          almMap[new_corr_c] = set()
+
+        if new_corr not in almMap[new_corr_c]:
+          almMap.setdefault(new_corr_c, []).add(new_corr)
 
   print()
 
+  for p in almMap:
+    corr_set = almMap[p]
 
-#pairs = relationToPairs(relation)
+    for corr in corr_set:
+      print("  ", p, set(corr[0]), " - ", set(corr[1]))
 
-#for p in pairs:
-#  print(p)
+    print()
+
+  print("-----------------------------------------------------------\n")
+
+
+
+
 
